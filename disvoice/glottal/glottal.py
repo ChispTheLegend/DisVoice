add202a3c19d98ee3cbe9ce6f05d5a8ea11d0012
Chris Song <92766351+ChispTheLegend@users.noreply.github.com>
2024-12-24 17:05:42 -0800

    Update glottal.py

--- disvoice/glottal/glottal.py
+++ disvoice/glottal/glottal.py
@@ -6,8 +6,7 @@ import numpy as np
 import pandas as pd
 import pysptk
 import torch
-#from scipy.integrate import cumtrapz
-from scipy.integrate._quadrature import cumtrapz
+from scipy.integrate import cumtrapz
 from scipy.io.wavfile import read
 from tqdm import tqdm
 
